c4a1b89ae808d091a5bbd1528a545e85af34b94d
Menu Temesgen <menutemesgen@gmail.com>
2025-04-09 15:30:49 +0300

    - I solved TopKFrequentElements problem from neetcode 250 list

---
+++ TopKFrequentElements.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+nums = [3,0,1,0]
+k = 1
+count = defaultdict(int)
+for i in nums:
+    count[i] += 1
+so_count = dict(sorted(count.items(), key=lambda item : item[1], reverse=True))
+ans = []
+for i, key in enumerate(so_count):
+    if i >= k:
+        break
+    ans.append(key)
+print(ans)
