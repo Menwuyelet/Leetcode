1322975adf97d9c78d6204a50a43dc350bf9b2be
Menu Temesgen <menutemesgen@gmail.com>
2025-11-02 13:18:15 +0300

    re solved valid palindrome using different approach.

--- ValidPalindrome.py
+++ ValidPalindrome.py
@@ -1,4 +1,4 @@
-s =  "Was it a car or a cat I saw?"
+s =   "A man, a plan, a canal: Panama"
 
 ## first solution 
 # filtered = "".join(char.lower() for char in s if char.isalnum())
@@ -17,17 +17,34 @@ s =  "Was it a car or a cat I saw?"
 # print (True)
 
 ## third solution 
+# left, right = 0, len(s)-1
 
+# while left < right:
+#     while not(s[left].isalnum()):
+#         left += 1
+#     while not(s[right].isalnum()):
+#         right -= 1
+    
+#     if s[left].lower() != s[right].lower():
+#         print(False)
+#         exit()
+#     left, right = left + 1, right - 1
+# print(True)
+
+## Fourth solution
 left, right = 0, len(s)-1
 
 while left < right:
-    while not(s[left].isalnum()):
-        left += 1
-    while not(s[right].isalnum()):
+    if (s[left].isalnum() and s[right].isalnum()) and s[left].lower() != s[right].lower():
+       x = s[left]
+       y = s[right]
+       print(False)
+       exit()
+    elif not(s[left].isalnum()):
+        left +=1
+    elif not(s[right].isalnum()):
         right -= 1
-    
-    if s[left].lower() != s[right].lower():
-        print(False)
-        exit()
-    left, right = left + 1, right - 1
+    else:
+        left, right = left + 1, right - 1
+
 print(True)
